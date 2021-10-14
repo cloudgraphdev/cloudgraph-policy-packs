71fd4d0b90692767829b425648eeca131706b85f
Marco Franceschi <franceschi.marco@gmail.com>
2021-10-14 11:53:00 -0300

    feat: Migrate engine to policy pack repository

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import RulesProvider from './provider'
+import {
+  rules,
+  ruleSchemaTypeName,
+  resourceTypeNamesToFieldsMap,
+} from './rules'
+
+export { rules, ruleSchemaTypeName, resourceTypeNamesToFieldsMap }
+
+export default RulesProvider
