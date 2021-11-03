8774124af243267ba75fbdae80eb2652d86a8a5e
Marco Franceschi <franceschi.marco@gmail.com>
2021-11-03 15:12:16 -0300

    feat: Added password policy requires minimum length of 14 or greater rule

--- src/index.ts
+++ src/index.ts
@@ -201,5 +201,22 @@ export default {
         equal: false,
       },
     },
+    {
+      id: 'r9',
+      description:
+        'AWS CIS 1.9 Ensure IAM password policy requires minimum length of 14 or greater',
+      gql: `{
+        queryawsIamPasswordPolicy {
+          id
+          __typename
+          minimumPasswordLength
+        }
+      }`,
+      resource: 'queryawsIamPasswordPolicy[*]',
+      conditions: {
+        path: '@.minimumPasswordLength',
+        lessThanInclusive: 14,
+      },
+    },
   ],
 }
