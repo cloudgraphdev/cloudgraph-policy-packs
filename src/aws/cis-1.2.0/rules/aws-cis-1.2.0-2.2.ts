ba1af5c094de3c63ac3b9e10dc5689991723c015
Marco Franceschi <franceschi.marco@gmail.com>
2021-11-25 15:36:31 -0300

    feat: Included 2.4 rule for cis 1.2.0

--- src/aws/cis-1.2.0/rules/aws-cis-1.2.0-2.2.ts
+++ src/aws/cis-1.2.0/rules/aws-cis-1.2.0-2.2.ts
@@ -1,5 +1,5 @@
 export default {
-  id: 'aws-cis-1.2.0-2.1',
+  id: 'aws-cis-1.2.0-2.2',
   description: 'AWS CIS 2.2 Ensure CloudTrail log file validation is enabled',
   gql: `{
     queryawsCloudtrail {
--- src/aws/cis-1.2.0/tests/aws-cis-1.2.0-2.x.test.ts
+++ src/aws/cis-1.2.0/tests/aws-cis-1.2.0-2.x.test.ts
@@ -3,6 +3,7 @@ import CloudGraph, { Rule, Engine } from '@cloudgraph/sdk'
 
 import Aws_CIS_120_21 from '../rules/aws-cis-1.2.0-2.1'
 import Aws_CIS_120_22 from '../rules/aws-cis-1.2.0-2.2'
+import Aws_CIS_120_24 from '../rules/aws-cis-1.2.0-2.4'
 
 describe('CIS Amazon Web Services Foundations: 1.2.0', () => {
   let rulesEngine: Engine
@@ -53,7 +54,7 @@ describe('CIS Amazon Web Services Foundations: 1.2.0', () => {
       expect(processedRule.result).toBe(CloudGraph.Result.PASS)
     })
 
-    test('Should pass when a trail has set multi region as true with all read-write type and include management events true', async () => {
+    test('Should fail when a trail has set multi region as true with all read-write type and include management events true', async () => {
       const data = {
         queryawsCloudtrail: [
           {
@@ -97,7 +98,7 @@ describe('CIS Amazon Web Services Foundations: 1.2.0', () => {
       expect(processedRule.result).toBe(CloudGraph.Result.PASS)
     })
 
-    test('Should pass when a trail has log file validation disabled', async () => {
+    test('Should fail when a trail has log file validation disabled', async () => {
       const data = {
         queryawsCloudtrail: [
           {
@@ -113,6 +114,67 @@ describe('CIS Amazon Web Services Foundations: 1.2.0', () => {
       )
 
       expect(processedRule.result).toBe(CloudGraph.Result.FAIL)
+    })
+  })
+
+  describe('AWS CIS 2.4 Ensure CloudTrail trails are integrated with CloudWatch Logs', () => {
+    test('Should pass when a trail has cloudwatch logs integrated with a delivery date no more than a day', async () => {
+      const data = {
+        queryawsCloudtrail: [
+          {
+            id: cuid(),
+            cloudWatchLogsLogGroupArn: cuid(),
+            status: {
+              latestCloudWatchLogsDeliveryTime: new Date().toISOString(),
+            },
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_CIS_120_24 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(CloudGraph.Result.PASS)
+    })
+
+    test('Should fail when a trail has cloudwatch logs integrated with a delivery date more than a day', async () => {
+      const data = {
+        queryawsCloudtrail: [
+          {
+            id: cuid(),
+            cloudWatchLogsLogGroupArn: cuid(),
+            status: {
+              latestCloudWatchLogsDeliveryTime: '2021-11-20T16:18:21.724Z',
+            },
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_CIS_120_24 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(CloudGraph.Result.FAIL)
+    })
+    test('Should pass when a trail does not have cloudwatch logs integrated', async () => {
+      const data = {
+        queryawsCloudtrail: [
+          {
+            id: cuid(),
+            cloudWatchLogsLogGroupArn: null,
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_CIS_120_24 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(CloudGraph.Result.PASS)
     })
   })
 })
