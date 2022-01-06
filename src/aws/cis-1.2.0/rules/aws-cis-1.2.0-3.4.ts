079fd773fb82f94e4e4f4fc16ea939a68575d54a
Marco Franceschi <franceschi.marco@gmail.com>
2022-01-06 17:21:19 -0300

    feat: Included rule 3.4 for AWS CIS

---
+++ src/aws/cis-1.2.0/rules/aws-cis-1.2.0-3.4.ts
@@ -0,0 +1,154 @@
+export default {
+  id: 'aws-cis-1.2.0-3.4',
+  description:
+    'AWS CIS 3.4 Ensure a log metric filter and alarm exist for IAM policy changes (Score)',
+  gql: `{
+    queryawsCloudtrail(filter: { isMultiRegionTrail: { eq: "Yes" } }) {
+      id
+      __typename
+      isMultiRegionTrail
+      status {
+        isLogging
+      }
+      eventSelectors {
+        id
+        readWriteType
+        includeManagementEvents
+      }
+      cloudwatchLog {
+        arn
+        metricFilters {
+          id
+          filterName
+          filterPattern
+        }
+        cloudwatch {
+          arn
+          actions
+          sns {
+            arn
+            subscriptions {
+              arn
+            }
+          }
+        }
+      }
+    }
+  }`,
+  resource: 'queryawsCloudtrail[*]',
+  severity: 'warning',
+  conditions: {
+    and: [
+      {
+        path: '@.isMultiRegionTrail',
+        equal: 'Yes',
+      },
+      {
+        path: '@.status.isLogging',
+        equal: true,
+      },
+      {
+        path: '@.eventSelectors',
+        array_any: {
+          and: [
+            { path: '[*].readWriteType', equal: 'All' },
+            {
+              path: '[*].includeManagementEvents',
+              equal: true,
+            },
+          ],
+        },
+      },
+      {
+        path: '@.cloudwatchLog',
+        array_any: {
+          path: '[*].metricFilters',
+          array_any: {
+            and: [
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DeleteGroupPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DeleteRolePolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DeleteUserPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*PutGroupPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*PutRolePolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*PutUserPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*CreatePolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DeletePolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*CreatePolicyVersion/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DeletePolicyVersion/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*AttachRolePolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DetachRolePolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*AttachUserPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DetachUserPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*AttachGroupPolicy/,
+              },
+              {
+                path: '[*].filterPattern',
+                match: /(\$.eventName)\s*=\s*DetachGroupPolicy/,
+              },
+            ],
+          },
+        },
+      },
+      {
+        path: '@.cloudwatchLog',
+        array_any: {
+          path: '[*].cloudwatch',
+          array_any: {
+            path: '[*].sns',
+            array_any: {
+              path: '[*].subscriptions',
+              array_any: {
+                path: '[*].arn',
+                notEqual: null,
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+}
