7ee3f793ce4185fbe7473bf4e6c74103a8bc5157
Marco Franceschi <franceschi.marco@gmail.com>
2022-01-06 17:22:01 -0300

    chore: Created unit tests for rule 3.4

--- src/aws/cis-1.2.0/tests/aws-cis-1.2.0-3.x.test.ts
+++ src/aws/cis-1.2.0/tests/aws-cis-1.2.0-3.x.test.ts
@@ -4,6 +4,7 @@ import 'jest'
 import Aws_CIS_120_31 from '../rules/aws-cis-1.2.0-3.1'
 import Aws_CIS_120_32 from '../rules/aws-cis-1.2.0-3.2'
 import Aws_CIS_120_33 from '../rules/aws-cis-1.2.0-3.3'
+import Aws_CIS_120_34 from '../rules/aws-cis-1.2.0-3.4'
 import Aws_CIS_120_310 from '../rules/aws-cis-1.2.0-3.10'
 import Aws_CIS_120_311 from '../rules/aws-cis-1.2.0-3.11'
 import Aws_CIS_120_312 from '../rules/aws-cis-1.2.0-3.12'
@@ -16,6 +17,9 @@ const Aws_CIS_120_32_Filter_Pattern =
   '{ ($.errorCode = "ConsoleLogin") || ($.additionalEventData.MFAUsed != "Yes")  }'
 const Aws_CIS_120_33_Filter_Pattern =
   '{ $.userIdentity.type = "Root" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != "AwsServiceEvent" }'
+const Aws_CIS_120_34_Filter_Pattern =
+  // eslint-disable-next-line max-len
+  '{ ($.eventName = DeleteGroupPolicy) || ($.eventName = DeleteRolePolicy) || ($.eventName = DeleteUserPolicy) || ($.eventName = PutGroupPolicy) || ($.eventName = PutRolePolicy) || ($.eventName = PutUserPolicy) || ($.eventName = CreatePolicy) || ($.eventName = DeletePolicy) || ($.eventName=CreatePolicyVersion) || ($.eventName=DeletePolicyVersion) || ($.eventName=AttachRolePolicy) || ($.eventName=DetachRolePolicy) || ($.eventName=AttachUserPolicy) || ($.eventName = DetachUserPolicy) || ($.eventName = AttachGroupPolicy) || ($.eventName = DetachGroupPolicy)}'
 const Aws_CIS_120_310_Filter_Pattern =
   // eslint-disable-next-line max-len
   '{ ($.eventName = AuthorizeSecurityGroupIngress) || ($.eventName = AuthorizeSecurityGroupEgress) || ($.eventName = RevokeSecurityGroupIngress) || ($.eventName = RevokeSecurityGroupEgress) || ($.eventName = CreateSecurityGroup) || ($.eventName = DeleteSecurityGroup) }'
@@ -298,7 +302,7 @@ describe('CIS Amazon Web Services Foundations: 1.2.0', () => {
         false
       await test33Rule(data, Result.FAIL)
     })
-    test('Security Issue when metricFilters filterPattern just contains one error code', async () => {
+    test('Security Issue when metricFilters filterPattern just contains one condition', async () => {
       const data = get3xValidResponse(
         '{ $.userIdentity.type = "Root" || $.userIdentity.invokedBy NOT EXISTS }'
       )
@@ -309,6 +313,54 @@ describe('CIS Amazon Web Services Foundations: 1.2.0', () => {
       data.queryawsCloudtrail[0].cloudwatchLog[0].cloudwatch[0].sns[0].subscriptions =
         []
       await test33Rule(data, Result.FAIL)
+    })
+  })
+
+  describe('AWS CIS 3.4 Ensure a log metric filter and alarm exist for IAM policy changes (Score)', () => {
+    const test34Rule = async (
+      data: CIS3xQueryResponse,
+      expectedResult: Result
+    ): Promise<void> => {
+      // Act
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_CIS_120_34 as Rule,
+        { ...data }
+      )
+
+      // Asserts
+      expect(processedRule.result).toBe(expectedResult)
+    }
+
+    test('No Security Issue when there are metric filters and alarms for security group changes', async () => {
+      const data = get3xValidResponse(Aws_CIS_120_34_Filter_Pattern)
+      await test34Rule(data, Result.PASS)
+    })
+
+    test('Security Issue when isLogging is false', async () => {
+      const data = get3xValidResponse(Aws_CIS_120_34_Filter_Pattern)
+      data.queryawsCloudtrail[0].status.isLogging = false
+      await test34Rule(data, Result.FAIL)
+    })
+    test('Security Issue when eventSelectors readWriteType is not All', async () => {
+      const data = get3xValidResponse(Aws_CIS_120_34_Filter_Pattern)
+      data.queryawsCloudtrail[0].eventSelectors[0].readWriteType = 'dummy'
+      await test34Rule(data, Result.FAIL)
+    })
+    test('Security Issue when eventSelectors includeManagementEvents is not true', async () => {
+      const data = get3xValidResponse(Aws_CIS_120_34_Filter_Pattern)
+      data.queryawsCloudtrail[0].eventSelectors[0].includeManagementEvents =
+        false
+      await test34Rule(data, Result.FAIL)
+    })
+    test('Security Issue when metricFilters filterPattern just contains one condition', async () => {
+      const data = get3xValidResponse('{ ($.eventName = DeleteGroupPolicy) }')
+      await test34Rule(data, Result.FAIL)
+    })
+    test('Security Issue when cloudwatch sns suscription is not found', async () => {
+      const data = get3xValidResponse(Aws_CIS_120_34_Filter_Pattern)
+      data.queryawsCloudtrail[0].cloudwatchLog[0].cloudwatch[0].sns[0].subscriptions =
+        []
+      await test34Rule(data, Result.FAIL)
     })
   })
 
