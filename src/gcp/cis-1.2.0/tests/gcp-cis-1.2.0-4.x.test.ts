6153dfae03ba7c93dd73ea803728455ea3a7d47a
Jesus Ibanez <jesus.ibanez@autocloud.dev>
2022-01-13 13:29:36 +0000

    feat: Included cis 4.1, 4.2, 4.3, 4.5, 4.6, 4.7 rules for gcp cis 1.2.0

--- src/gcp/cis-1.2.0/tests/gcp-cis-1.2.0-4.x.test.ts
+++ src/gcp/cis-1.2.0/tests/gcp-cis-1.2.0-4.x.test.ts
@@ -73,6 +73,7 @@ export interface QuerygcpVmInstance {
   serviceAccounts?: ServiceAccount[]
   disks?: Disk[]
 }
+
 export interface CIS4xQueryResponse {
   querygcpVmInstance?: QuerygcpVmInstance[]
 }
