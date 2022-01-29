97c5ac76813eaf937de4e7dc51cd979a110729b5
Tyson Kunovsky <tyson@autocloud.dev>
2022-01-28 18:50:07 -0600

    feat: CIS info for rules up to 4.3

--- src/aws/cis-1.2.0/rules/aws-cis-1.2.0-4.2.ts
+++ src/aws/cis-1.2.0/rules/aws-cis-1.2.0-4.2.ts
@@ -2,9 +2,29 @@ export default {
   id: 'aws-cis-1.2.0-4.2',
   description:
     'AWS CIS 4.2 Ensure no security groups allow ingress from 0.0.0.0/0 to port 3389 (Scored)',
-  audit: ``,
-  rationale: ``,
-  remediation: ``,
+  audit: `Perform the following to determine if the account is configured as prescribed:
+
+  1. Login to the AWS Management Console at
+      https://console.aws.amazon.com/vpc/home
+  2. In the left pane, click Security Groups
+  3. For each security group, perform the following:
+  4. Select the security group
+  5. Click the Inbound Rules tab
+  6. Ensure no rule exists that has a port range that includes port 3389 and has a Source of 0.0.0.0/0
+  
+  Note: A Port value of ALL or a port range such as 1024 - 4098 are inclusive of port 3389.`,
+  rationale: `Removing unfettered connectivity to remote console services, such as RDP, reduces a server's exposure to risk.`,
+  remediation: `Perform the following to implement the prescribed state:
+
+  1. Login to the AWS Management Console at
+      https://console.aws.amazon.com/vpc/home
+  2. In the left pane, click Security Groups
+  3. For each security group, perform the following:
+  4. Select the security group
+  5. Click the Inbound Rules tab
+  6. Identify the rules to be removed
+  7. Click the x in the Remove column
+  8. Click Save`,
   references: [],
   gql: `{
     queryawsSecurityGroup{
