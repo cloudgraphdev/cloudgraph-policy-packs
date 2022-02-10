7b9cc541b6dee8b758f6f468375cc76a5be0c225
Marco Franceschi <franceschi.marco@gmail.com>
2022-02-10 18:44:46 -0300

    feat: Included cloudtrail-check-2 rule for PCI DSS

--- src/aws/pci-dss-3.2.1/tests/pci-dss-3.2.1-cloudtrail-checks.test.ts
+++ src/aws/pci-dss-3.2.1/tests/pci-dss-3.2.1-cloudtrail-checks.test.ts
@@ -2,6 +2,7 @@ import cuid from 'cuid'
 import CloudGraph, { Rule, Result, Engine } from '@cloudgraph/sdk'
 
 import Aws_PCI_DSS_321_Cloudtrail_1 from '../rules/pci-dss-3.2.1-cloudtrail-check-1'
+import Aws_PCI_DSS_321_Cloudtrail_2 from '../rules/pci-dss-3.2.1-cloudtrail-check-2'
 
 describe('PCI Data Security Standard: 3.2.1', () => {
   let rulesEngine: Engine
@@ -40,6 +41,80 @@ describe('PCI Data Security Standard: 3.2.1', () => {
 
       const [processedRule] = await rulesEngine.processRule(
         Aws_PCI_DSS_321_Cloudtrail_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.PASS)
+    })
+  })
+
+  describe('CloudTrail Check 2: CloudTrail should be enabled', () => {
+    test('Should fail when the Trail is not multi-region', async () => {
+      const data = {
+        queryawsCloudtrail: [
+          {
+            id: cuid(),
+            isMultiRegionTrail: "No",
+            eventSelectors: [
+              {
+                readWriteType: "All",
+                includeManagementEvents: true
+              }
+            ]
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Cloudtrail_2 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.FAIL)
+    })
+
+    test('Should fail when the Trail is multi-region but the event selectors are not configured correctly', async () => {
+      const data = {
+        queryawsCloudtrail: [
+          {
+            id: cuid(),
+            isMultiRegionTrail: "Yes",
+            eventSelectors: [
+              {
+                readWriteType: "ReadOnly",
+                includeManagementEvents: false
+              }
+            ]
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Cloudtrail_2 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.FAIL)
+    })
+
+    test('Should pass when the Trail is multi-region and the event selectors are configured correctly', async () => {
+      const data = {
+        queryawsCloudtrail: [
+          {
+            id: cuid(),
+            isMultiRegionTrail: "Yes",
+            eventSelectors: [
+              {
+                readWriteType: "All",
+                includeManagementEvents: true
+              }
+            ]
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Cloudtrail_2 as Rule,
         { ...data } as any
       )
 
