ac4ad2f2f76893da4cf201968a3653aafbe91e9b
Christopher Brandt <christopher.brandt@autocloud.dev>
2022-02-17 08:57:43 -0800

    fix(gcp-cis-1.2.0-1.14): add rationale

--- src/gcp/cis-1.2.0/rules/gcp-cis-1.2.0-1.14.ts
+++ src/gcp/cis-1.2.0/rules/gcp-cis-1.2.0-1.14.ts
@@ -4,6 +4,18 @@ export default {
   description: `API keys are insecure because they can be viewed publicly, such as from within a browser,
   or they can be accessed on a device where the key resides. It is recommended to restrict
   API keys to use (call) only APIs required by an application.`,
+  rationale: `Security risks involved in using API-Keys are below:
+* API keys are simple encrypted strings
+* API keys do not identify the user or the application making the API request
+* API keys are typically accessible to clients, making it easy to discover and steal an
+API key
+In light of these potential risks, Google recommends using the standard authentication flow
+instead of API-Keys. However, there are limited cases where API keys are more
+appropriate. For example, if there is a mobile application that needs to use the Google
+Cloud Translation API, but doesn't otherwise need a backend server, API keys are the
+simplest way to authenticate to that API.
+In order to reduce attack surfaces by providing least privileges, API-Keys can be
+restricted to use (call) only APIs required by an application.`,
   audit: `**From Console:**
 1. Go to APIs & Services\\Credentials using https://console.cloud.google.com/apis/credentials
 2. In the section API Keys, Click the API Key Name. The API Key properties display on a
