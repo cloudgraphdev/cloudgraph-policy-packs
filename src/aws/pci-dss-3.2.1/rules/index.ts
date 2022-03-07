70ffae034fb858ba6233ad763dc288198d06bd4e
tyler dunkel <tyler.dunkel@autocloud.dev>
2022-03-07 10:58:36 -0600

    fix(checks): export checks from index file

--- src/aws/pci-dss-3.2.1/rules/index.ts
+++ src/aws/pci-dss-3.2.1/rules/index.ts
@@ -1,5 +1,6 @@
 import Aws_PCI_DSS_321_Autoscaling_1 from './pci-dss-3.2.1-autoscaling-check-1'
 import Aws_PCI_DSS_321_Alb_1 from './pci-dss-3.2.1-alb-check-1'
+import Aws_PCI_DSS_321_Cloudfront_1 from './pci-dss-3.2.1-cloudfront-check-1'
 import Aws_PCI_DSS_321_Codebuild_1 from './pci-dss-3.2.1-codebuild-check-1'
 import Aws_PCI_DSS_321_Codebuild_2 from './pci-dss-3.2.1-codebuild-check-2'
 import Aws_PCI_DSS_321_Cloudtrail_1 from './pci-dss-3.2.1-cloudtrail-check-1'
@@ -18,6 +19,7 @@ import Aws_PCI_DSS_321_EC2_6 from './pci-dss-3.2.1-ec2-check-6'
 export default [
   Aws_PCI_DSS_321_Autoscaling_1,
   Aws_PCI_DSS_321_Alb_1,
+  Aws_PCI_DSS_321_Cloudfront_1,
   Aws_PCI_DSS_321_Codebuild_1,
   Aws_PCI_DSS_321_Codebuild_2,
   Aws_PCI_DSS_321_Cloudtrail_1,
