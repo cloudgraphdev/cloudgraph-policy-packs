e6e62833851fdb1c852ba07fb23064438d96cd1b
tyler dunkel <tyler.dunkel@autocloud.dev>
2022-03-07 11:15:07 -0600

    feat(checks): new check dms1 for pci

---
+++ src/aws/pci-dss-3.2.1/tests/pci-dss-3.2.1-dms-checks.test.ts
@@ -0,0 +1,52 @@
+import cuid from 'cuid'
+import CloudGraph, { Rule, Result, Engine } from '@cloudgraph/sdk'
+
+import Aws_PCI_DSS_321_Dms_1 from '../rules/pci-dss-3.2.1-dms-check-1'
+
+describe('PCI Data Security Standard: 3.2.1', () => {
+  let rulesEngine: Engine
+  beforeAll(() => {
+    rulesEngine = new CloudGraph.RulesEngine({
+      providerName: 'aws',
+      entityName: 'PCI',
+    })
+  })
+
+  describe('DMS Check 1: AWS Database Migration Service replication instances should not be public', () => {
+    test('Should pass when it is not publically accessible', async () => {
+      const data = {
+        queryawsDmsReplicationInstance: [
+          {
+            id: cuid(),
+            publiclyAccessible: false
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Dms_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.PASS)
+    })
+
+    test('Should fail when it is publically accessible', async () => {
+      const data = {
+        queryawsDmsReplicationInstance: [
+          {
+            id: cuid(),
+            publiclyAccessible: true
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Dms_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.FAIL)
+    })
+  })
+})
