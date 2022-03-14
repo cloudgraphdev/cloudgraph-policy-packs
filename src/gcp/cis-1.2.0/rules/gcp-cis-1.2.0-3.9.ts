b91265a59adf2291a7cc8c49e55cccc5156a59cf
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-03-14 12:25:07 -0300

    feat: GCP rule 3.9 could present a false failure if a Policy is insecure, but it's not used by a proxy
    feat: GCP rule 3.9 could present a false failure if a Policy is insecure, but it's not used by a proxy

--- src/gcp/cis-1.2.0/rules/gcp-cis-1.2.0-3.9.ts
+++ src/gcp/cis-1.2.0/rules/gcp-cis-1.2.0-3.9.ts
@@ -1,3 +1,60 @@
+const targetHttpsAndSslProxyConditions = {
+  and: [
+    {
+      path: '@.sslPolicy',
+      isEmpty: false,
+    },
+    {
+      path: '@.sslPolicy',
+      array_all: {
+        or: [
+          {
+            and: [
+              {
+                path: '[*].profile',
+                equal: 'MODERN',
+              },
+              {
+                path: '[*].minTlsVersion',
+                equal: 'TLS_1_2',
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                path: '[*].profile',
+                equal: 'RESTRICTED',
+              },
+            ],
+          },
+          {
+            and: [
+              {
+                path: '[*].profile',
+                equal: 'CUSTOM',
+              },
+              {
+                path: '[*].enabledFeatures',
+                array_all: {
+                  path: '[*]',
+                  notIn: [
+                    'TLS_RSA_WITH_AES_128_GCM_SHA256',
+                    'TLS_RSA_WITH_AES_256_GCM_SHA384',
+                    'TLS_RSA_WITH_AES_128_CBC_SHA',
+                    'TLS_RSA_WITH_AES_256_CBC_SHA',
+                    'TLS_RSA_WITH_3DES_EDE_CBC_SHA',
+                  ],
+                },
+              },
+            ],
+          },
+        ],
+      },
+    }
+  ]
+}
+
 export default {
   id: 'gcp-cis-1.2.0-3.9',
   title:
@@ -71,7 +128,7 @@ export default {
           TLS_RSA_WITH_AES_128_CBC_SHA
           TLS_RSA_WITH_AES_256_CBC_SHA
           TLS_RSA_WITH_3DES_EDE_CBC_SHA`,
-  rationale: `Load balancers are used to efficiently distribute traffic across multiple servers. Both SSL proxy and HTTPS load balancers are external load balancers, meaning they distribute traffic from the Internet to a GCP network. GCP customers can configure load balancer SSL policies with a minimum TLS version (1.0, 1.1, or 1.2) that clients can use to establish a connection, along with a profile (Compatible, Modern, Restricted, or Custom) that specifies permissible cipher suites. To comply with users using outdated protocols, GCP load balancers can be configured to permit insecure cipher suites. In fact, the GCP default SSL policy uses a minimum TLS version of 1.0 and a Compatible profile, which allows the widest range of insecure cipher suites. As a result, it is easy for customers to configure a load balancer without even knowing that they are permitting outdated cipher suites.`,
+  rationale: 'Load balancers are used to efficiently distribute traffic across multiple servers. Both SSL proxy and HTTPS load balancers are external load balancers, meaning they distribute traffic from the Internet to a GCP network. GCP customers can configure load balancer SSL policies with a minimum TLS version (1.0, 1.1, or 1.2) that clients can use to establish a connection, along with a profile (Compatible, Modern, Restricted, or Custom) that specifies permissible cipher suites. To comply with users using outdated protocols, GCP load balancers can be configured to permit insecure cipher suites. In fact, the GCP default SSL policy uses a minimum TLS version of 1.0 and a Compatible profile, which allows the widest range of insecure cipher suites. As a result, it is easy for customers to configure a load balancer without even knowing that they are permitting outdated cipher suites.',
   remediation: `**From Console:**
   If the TargetSSLProxy or TargetHttpsProxy does not have an SSL policy configured, create a new SSL policy. Otherwise, modify the existing insecure policy.
 
@@ -101,102 +158,42 @@ export default {
           gcloud compute target-https-proxies update TARGET_HTTPS_POLICY_NAME --ssl- policy SSL_POLICY_NAME
   `,
   references: [
-    `https://cloud.google.com/load-balancing/docs/use-ssl-policies`,
-    `https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-52r2.pdf`,
+    'https://cloud.google.com/load-balancing/docs/use-ssl-policies',
+    'https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-52r2.pdf',
   ],
-  gql: `{
-    querygcpProject{
-      id
-      projectId
-      __typename
-      targetHttpsProxies{
-        id
-        name
-        sslPolicy
-      }
-      targetSslProxies{
-        id
-        name
-        sslPolicy
-      }
-      sslPolicies{
-        profile
-        enabledFeatures
-        minTlsVersion
-        selfLink
-      }
-    }
-  }`,
-  resource: 'querygcpProject[*]',
-  severity: 'unknown',
-  conditions: {
-    path: '@',
-    jq: `{
-      "id": .id,
-      "targetHttpsProxiesAndTargetSslProxies" : (
-        [(.targetHttpsProxies + .targetSslProxies)[] + {"targetSslPolicy" :  .sslPolicies }]
-        | map(. as $proxy | ($proxy.targetSslPolicy |= (select($proxy.sslPolicy == $proxy.targetSslPolicy[].selfLink) // null )[0]) )
-        )
-    }
-    `,
-    and: [
-      {
-        path: '@.targetHttpsProxiesAndTargetSslProxies',
-        array_all: {
-          and: [
-            {
-              path: '[*].targetSslPolicy',
-              notEqual: null,
-            },
-            {
-              path: '[*].targetSslPolicy',
-              or: [
-                {
-                  and: [
-                    {
-                      path: '[*].profile',
-                      equal: 'MODERN',
-                    },
-                    {
-                      path: '[*].minTlsVersion',
-                      equal: 'TLS_1_2',
-                    },
-                  ],
-                },
-                {
-                  and: [
-                    {
-                      path: '[*].profile',
-                      equal: 'RESTRICTED',
-                    },
-                  ],
-                },
-                {
-                  and: [
-                    {
-                      path: '[*].profile',
-                      equal: 'CUSTOM',
-                    },
-                    {
-                      path: '[*].enabledFeatures',
-                      array_all: {
-                        path: '[*]',
-                        notIn: [
-                          'TLS_RSA_WITH_AES_128_GCM_SHA256',
-                          'TLS_RSA_WITH_AES_256_GCM_SHA384',
-                          'TLS_RSA_WITH_AES_128_CBC_SHA',
-                          'TLS_RSA_WITH_AES_256_CBC_SHA',
-                          'TLS_RSA_WITH_3DES_EDE_CBC_SHA',
-                        ],
-                      },
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      },
-    ],
-  },
+  severity: 'medium',
+  queries: [
+    {
+      gql: `{
+        querygcpTargetHttpsProxy {
+          id
+          projectId
+          __typename
+          sslPolicy {
+            profile
+            enabledFeatures
+            minTlsVersion
+          }
+        }
+      }`,
+      resource: 'querygcpTargetHttpsProxy[*]', 
+      conditions: targetHttpsAndSslProxyConditions
+    },
+    {
+      gql: `{
+        querygcpTargetSslProxy {
+          id
+          projectId
+          __typename
+          sslPolicy {
+            profile
+            enabledFeatures
+            minTlsVersion
+          }
+        }
+      }`,
+      resource: 'querygcpTargetSslProxy[*]', 
+      conditions: targetHttpsAndSslProxyConditions
+    },
+  ],
 }
