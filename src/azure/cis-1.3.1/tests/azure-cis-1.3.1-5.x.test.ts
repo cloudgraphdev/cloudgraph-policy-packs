5a5b207270cadf0179c3a55526126c37b84639c6
Harold <harold.araujo@autocloud.dev>
2022-03-16 16:15:54 -0300

    feat(checks): Included 5.1.x rules for azure cis 1.3.1

--- src/azure/cis-1.3.1/tests/azure-cis-1.3.1-5.x.test.ts
+++ src/azure/cis-1.3.1/tests/azure-cis-1.3.1-5.x.test.ts
@@ -1,379 +1,185 @@
 /* eslint-disable max-len */
 import cuid from 'cuid'
-import CloudGraph, { Rule, Result, Engine } from '@cloudgraph/sdk'
+import { Rule, Result, Engine } from '@cloudgraph/sdk'
 import 'jest'
 
-import Azure_CIS_131_521 from '../rules/azure-cis-1.3.1-5.2.1'
-import Azure_CIS_131_522 from '../rules/azure-cis-1.3.1-5.2.2'
-import Azure_CIS_131_523 from '../rules/azure-cis-1.3.1-5.2.3'
-import Azure_CIS_131_524 from '../rules/azure-cis-1.3.1-5.2.4'
-import Azure_CIS_131_525 from '../rules/azure-cis-1.3.1-5.2.5'
-import Azure_CIS_131_526 from '../rules/azure-cis-1.3.1-5.2.6'
+import Azure_CIS_131_152 from '../rules/azure-cis-1.3.1-5.1.2'
+import Azure_CIS_131_153 from '../rules/azure-cis-1.3.1-5.1.3'
+import Azure_CIS_131_154 from '../rules/azure-cis-1.3.1-5.1.4'
+import { initRuleEngine, testRule } from './utils'
 
-export interface azureActivityLogAlertAllOfCondition {
-  allOf: [azureActivityLogAlertLeafCondition]
+export interface QueryazureStorageAccountData {
+  encryptionKeySource: string
+  storageContainers: Array<{
+    name: string
+  }>
 }
 
-export interface azureActivityLogAlertLeafCondition {
+export interface QueryazureStorageContainer {
   id: string
-  field: string
-  equals: string
+  name: string
+  publicAccess?: string
+  storageAccount?: QueryazureStorageAccountData
+}
+export interface QueryazureDiagnosticSettingLog {
+  category: string
+  enabled: boolean
+  retentionPolicyEnabled: boolean
+  retentionPolicyDays: number | null
 }
 
-export interface QueryazureActivityLogAlert {
+export interface QueryazureDiagnosticSetting {
   id: string
-  region?: string
-  enabled?: boolean
-  condition?: azureActivityLogAlertAllOfCondition
-}
-export interface QueryazureResourceGroup {
-  id: string
-  activityLogAlerts: QueryazureActivityLogAlert[]
+  appropiateCategories?: boolean
+  logs?: QueryazureDiagnosticSettingLog[]
 }
 
-export interface CIS5xQueryResponse {
-  queryazureActivityLogAlert?: QueryazureActivityLogAlert[]
-  queryazureResourceGroup?: QueryazureResourceGroup[]
+export interface CIS51xQueryResponse {
+  queryazureStorageContainer?: QueryazureStorageContainer[]
+  queryazureDiagnosticSetting?: QueryazureDiagnosticSetting[]
+}
+
+type CIS51xQueryType =
+  | 'queryazureStorageContainer'
+  | 'queryazureDiagnosticSetting'
+
+const getTestRuleFixture = ({
+  queryType,
+  name = 'whatever',
+  encryptionKeySource,
+  publicAccess,
+  appropiateCategories,
+  storageContainers = [],
+  logs = [],
+}: {
+  queryType?: CIS51xQueryType
+  name?: string
+  encryptionKeySource?: string
+  publicAccess?: string
+  appropiateCategories?: boolean
+  storageContainers?: Array<{
+    name: string
+  }>
+  logs?: QueryazureDiagnosticSettingLog[]
+}): CIS51xQueryResponse => {
+  const result: CIS51xQueryResponse = {}
+  result.queryazureDiagnosticSetting = []
+  result.queryazureStorageContainer = []
+  if (queryType === 'queryazureStorageContainer') {
+    result.queryazureStorageContainer.push({
+      id: cuid(),
+      name,
+      publicAccess,
+      ...(encryptionKeySource
+        ? {
+            storageAccount: {
+              encryptionKeySource,
+              storageContainers,
+            },
+          }
+        : {}),
+    })
+  }
+  if (queryType === 'queryazureDiagnosticSetting') {
+    result.queryazureDiagnosticSetting.push({
+      id: cuid(),
+      appropiateCategories,
+      logs,
+    })
+  }
+  return result
 }
 
 describe('CIS Microsoft Azure Foundations: 1.3.1', () => {
   let rulesEngine: Engine
   beforeAll(() => {
-    rulesEngine = new CloudGraph.RulesEngine({ providerName: 'azure', entityName: 'CIS'} )
+    rulesEngine = initRuleEngine('azure', 'CIS')
   })
 
-  describe('Azure CIS 5.2.1 Ensure that Activity Log Alert exists for Create Policy Assignment', () => {
-    const getTestRuleFixture = (
-      region: string,
-      enabled: boolean,
-      field: string,
-      equals: string,
-    ): CIS5xQueryResponse => {
-      return {
-        queryazureResourceGroup: [
-          {
-            id: cuid(),
-            activityLogAlerts: [{
-              id: cuid(),
-              region,
-              enabled,
-              condition: {
-                allOf: [{
-                  id: cuid(),
-                  field,
-                  equals,
-                }]
-              },
-            }],
-          },
-        ],
-      }
-    }
-
-    const testRule = async (
-      data: CIS5xQueryResponse,
-      expectedResult: Result
-    ): Promise<void> => {
-      // Act
-      const [processedRule] = await rulesEngine.processRule(
-        Azure_CIS_131_521 as Rule,
-        { ...data }
-      )
-
-      // Asserts
-      expect(processedRule.result).toBe(expectedResult)
-    }
-
-    test('No Security Issue when Activity Log Alert exists for Create Policy Assignment', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, 'operationName', 'microsoft.authorization/policyassignments/write')
-
-      await testRule(data, Result.PASS)
+  describe('Azure 5.1.2 Ensure Diagnostic Setting captures appropriate categories', () => {
+    test('Checks any if insights-activity-logs is found and has the appropiate categories set', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureDiagnosticSetting',
+        name: 'insights-activity-logs',
+        appropiateCategories: true,
+        storageContainers: [{ name: 'insights-activity-logs' }],
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_152 as Rule, Result.PASS)
     })
 
+    test('Checks any if insights-activity-logs is found and but does not have the appropiate categories set', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureDiagnosticSetting',
+        name: 'insights-activity-logs',
+        appropiateCategories: false,
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_152 as Rule, Result.FAIL)
+    })
 
-    test('Security Issue when Activity Log Alert doesnt exist for Create Policy Assignment', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, '', '')
-
-      await testRule(data, Result.FAIL)
+    test('No settings found', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureDiagnosticSetting',
+        appropiateCategories: false,
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_152 as Rule, Result.FAIL)
     })
   })
 
-  describe('Azure CIS 5.2.2 Ensure that Activity Log Alert exists for Delete Policy Assignment', () => {
-    const getTestRuleFixture = (
-      region: string,
-      enabled: boolean,
-      field: string,
-      equals: string,
-    ): CIS5xQueryResponse => {
-      return {
-        queryazureResourceGroup: [
-          {
-            id: cuid(),
-            activityLogAlerts: [{
-              id: cuid(),
-              region,
-              enabled,
-              condition: {
-                allOf: [{
-                  id: cuid(),
-                  field,
-                  equals,
-                }]
-              },
-            }],
-          },
-        ],
-      }
-    }
-
-    const testRule = async (
-      data: CIS5xQueryResponse,
-      expectedResult: Result
-    ): Promise<void> => {
-      // Act
-      const [processedRule] = await rulesEngine.processRule(
-        Azure_CIS_131_522 as Rule,
-        { ...data }
-      )
-
-      // Asserts
-      expect(processedRule.result).toBe(expectedResult)
-    }
-
-    test('No Security Issue when Activity Log Alert exists for Delete Policy Assignment', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, 'operationName', 'microsoft.authorization/policyassignments/delete')
-
-      await testRule(data, Result.PASS)
+  describe('Azure 5.1.3 Ensure the storage container storing the activity logs is not publicly accessible', () => {
+    test('Finds insights-operational-logs storage container and checks that publicAccess prop equals "None"', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureStorageContainer',
+        name: 'insights-operational-logs',
+        publicAccess: 'None',
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_153 as Rule, Result.PASS)
     })
 
+    test('Finds insights-operational-logs storage container and fails checks because publicAccess is set to "Storage', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureStorageContainer',
+        name: 'insights-operational-logs',
+        publicAccess: 'Storage',
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_153 as Rule, Result.FAIL)
+    })
 
-    test('Security Issue when Activity Log Alert doesnt exist for Delete Policy Assignment', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, '', '')
-
-      await testRule(data, Result.FAIL)
+    test('Container not found', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureStorageContainer',
+        name: 'scm-releases',
+        publicAccess: 'Storage',
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_153 as Rule, Result.FAIL)
     })
   })
 
-  describe('Azure CIS 5.2.3 Ensure that Activity Log Alert exists for Create or Update Network Security Group', () => {
-    const getTestRuleFixture = (
-      region: string,
-      enabled: boolean,
-      field: string,
-      equals: string,
-    ): CIS5xQueryResponse => {
-      return {
-        queryazureResourceGroup: [
-          {
-            id: cuid(),
-            activityLogAlerts: [{
-              id: cuid(),
-              region,
-              enabled,
-              condition: {
-                allOf: [{
-                  id: cuid(),
-                  field,
-                  equals,
-                }]
-              },
-            }],
-          },
-        ],
-      }
-    }
-
-    const testRule = async (
-      data: CIS5xQueryResponse,
-      expectedResult: Result
-    ): Promise<void> => {
-      // Act
-      const [processedRule] = await rulesEngine.processRule(
-        Azure_CIS_131_523 as Rule,
-        { ...data }
-      )
-
-      // Asserts
-      expect(processedRule.result).toBe(expectedResult)
-    }
-
-    test('No Security Issue when Activity Log Alert exists for Create or Update Network Security Group', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, 'operationName', 'microsoft.network/networksecuritygroups/write')
-
-      await testRule(data, Result.PASS)
+  describe('Azure 5.1.4 Ensure the storage account containing the container with activity logs is encrypted with BYOK (Use Your Own Key)', () => {
+    test('Finds insights-operational-logs storage container and storage account is encrypted with BYOK', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureStorageContainer',
+        name: 'insights-operational-logs',
+        encryptionKeySource: 'Microsoft.Keyvault',
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_154 as Rule, Result.PASS)
     })
 
-
-    test('Security Issue when Activity Log Alert doesnt exist for Create or Update Network Security Group', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, '', '')
-
-      await testRule(data, Result.FAIL)
-    })
-  })
-
-  describe('Azure CIS 5.2.4 Ensure that Activity Log Alert exists for Delete Network Security Group', () => {
-    const getTestRuleFixture = (
-      region: string,
-      enabled: boolean,
-      field: string,
-      equals: string,
-    ): CIS5xQueryResponse => {
-      return {
-        queryazureResourceGroup: [
-          {
-            id: cuid(),
-            activityLogAlerts: [{
-              id: cuid(),
-              region,
-              enabled,
-              condition: {
-                allOf: [{
-                  id: cuid(),
-                  field,
-                  equals,
-                }]
-              },
-            }],
-          },
-        ],
-      }
-    }
-
-    const testRule = async (
-      data: CIS5xQueryResponse,
-      expectedResult: Result
-    ): Promise<void> => {
-      // Act
-      const [processedRule] = await rulesEngine.processRule(
-        Azure_CIS_131_524 as Rule,
-        { ...data }
-      )
-
-      // Asserts
-      expect(processedRule.result).toBe(expectedResult)
-    }
-
-    test('No Security Issue when Activity Log Alert exists for Delete Network Security Group', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, 'operationName', 'microsoft.network/networksecuritygroups/delete')
-
-      await testRule(data, Result.PASS)
+    test('Finds insights-operational-logs storage container and storage account is encrypted by system managed key(Storage)', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureStorageContainer',
+        name: 'insights-operational-logs',
+        encryptionKeySource: 'Microsoft.Storage',
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_154 as Rule, Result.FAIL)
     })
 
-    test('Security Issue when Activity Log Alert doesnt exist for Delete Network Security Group', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, '', '')
-
-      await testRule(data, Result.FAIL)
-    })
-  })
-
-  describe('Azure CIS 5.2.5 Ensure that Activity Log Alert exists for Create or Update Network Security Group Rule', () => {
-    const getTestRuleFixture = (
-      region: string,
-      enabled: boolean,
-      field: string,
-      equals: string,
-    ): CIS5xQueryResponse => {
-      return {
-        queryazureResourceGroup: [
-          {
-            id: cuid(),
-            activityLogAlerts: [{
-              id: cuid(),
-              region,
-              enabled,
-              condition: {
-                allOf: [{
-                  id: cuid(),
-                  field,
-                  equals,
-                }]
-              },
-            }],
-          },
-        ],
-      }
-    }
-
-    const testRule = async (
-      data: CIS5xQueryResponse,
-      expectedResult: Result
-    ): Promise<void> => {
-      // Act
-      const [processedRule] = await rulesEngine.processRule(
-        Azure_CIS_131_525 as Rule,
-        { ...data }
-      )
-
-      // Asserts
-      expect(processedRule.result).toBe(expectedResult)
-    }
-
-    test('No Security Issue when Activity Log Alert exists for Create or Update Network Security Group Rule', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, 'operationName', 'microsoft.network/networksecuritygroups/securityrules/write')
-
-      await testRule(data, Result.PASS)
-    })
-
-
-    test('Security Issue when Activity Log Alert doesnt exist for Create or Update Network Security Group Rule', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, '', '')
-
-      await testRule(data, Result.FAIL)
-    })
-  })
-
-  describe('Azure CIS 5.2.6 Ensure that Activity Log Alert exists for the Delete Network Security Group Rule', () => {
-    const getTestRuleFixture = (
-      region: string,
-      enabled: boolean,
-      field: string,
-      equals: string,
-    ): CIS5xQueryResponse => {
-      return {
-        queryazureResourceGroup: [
-          {
-            id: cuid(),
-            activityLogAlerts: [{
-              id: cuid(),
-              region,
-              enabled,
-              condition: {
-                allOf: [{
-                  id: cuid(),
-                  field,
-                  equals,
-                }]
-              },
-            }],
-          },
-        ],
-      }
-    }
-
-    const testRule = async (
-      data: CIS5xQueryResponse,
-      expectedResult: Result
-    ): Promise<void> => {
-      // Act
-      const [processedRule] = await rulesEngine.processRule(
-        Azure_CIS_131_526 as Rule,
-        { ...data }
-      )
-
-      // Asserts
-      expect(processedRule.result).toBe(expectedResult)
-    }
-
-    test('No Security Issue when Activity Log Alert exists for the Delete Network Security Group Rule', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, 'operationName', 'microsoft.network/networksecuritygroups/securityrules/delete')
-
-      await testRule(data, Result.PASS)
-    })
-
-
-    test('Security Issue when Activity Log Alert doesnt exist for the Delete Network Security Group Rule', async () => {
-      const data: CIS5xQueryResponse = getTestRuleFixture('global', true, '', '')
-
-      await testRule(data, Result.FAIL)
+    test('Container not found', async () => {
+      const data: CIS51xQueryResponse = getTestRuleFixture({
+        queryType: 'queryazureStorageContainer',
+        name: 'scm-releases',
+        encryptionKeySource: 'Microsoft.Storage',
+      })
+      await testRule(rulesEngine, data, Azure_CIS_131_154 as Rule, Result.FAIL)
     })
   })
 })
