f0061d12405ce6c24525a632d77270545d0f4623
tyler dunkel <tyler.dunkel@autocloud.dev>
2022-03-23 11:08:00 -0500

    feat(checks): new check lambda-check-1

--- src/aws/pci-dss-3.2.1/tests/pci-dss-3.2.1-lambda-checks.test.ts
+++ src/aws/pci-dss-3.2.1/tests/pci-dss-3.2.1-lambda-checks.test.ts
@@ -1,6 +1,7 @@
 import cuid from 'cuid'
 import CloudGraph, { Rule, Result, Engine } from '@cloudgraph/sdk'
 
+import Aws_PCI_DSS_321_Lambda_1 from '../rules/pci-dss-3.2.1-lambda-check-1'
 import Aws_PCI_DSS_321_Lambda_2 from '../rules/pci-dss-3.2.1-lambda-check-2'
 
 describe('PCI Data Security Standard: 3.2.1', () => {
@@ -12,6 +13,145 @@ describe('PCI Data Security Standard: 3.2.1', () => {
     })
   })
 
+  describe('Lambda Check 1: Lambda functions should prohibit public access', () => {
+    test('Should pass when the policy does not exist', async () => {
+      const data = {
+        queryawsLambda: [
+          {
+            id: cuid(),
+            policy: null
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Lambda_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.PASS)
+    })
+
+    test('Should pass when the policy does not allow public access', async () => {
+      const data = {
+        queryawsLambda: [
+          {
+            id: cuid(),
+            policy: {
+              statement: [
+                {
+                  effect: 'Allow',
+                  principal: [
+                    {
+                      key: 'Service',
+                      value: ['lambda.amazonaws.com'],
+                    },
+                  ],
+                },
+              ],
+            }
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Lambda_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.PASS)
+    })
+
+    test('Should fail when the policy has a * principal', async () => {
+      const data = {
+        queryawsLambda: [
+          {
+            id: cuid(),
+            policy: {
+              statement: [
+                {
+                  effect: 'Allow',
+                  principal: [
+                    {
+                      key: '',
+                      value: ['*'],
+                    },
+                  ],
+                },
+              ],
+            }
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Lambda_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.FAIL)
+    })
+
+    test('Should fail when the policy has a {AWS: `*`} principal', async () => {
+      const data = {
+        queryawsLambda: [
+          {
+            id: cuid(),
+            policy: {
+              statement: [
+                {
+                  effect: 'Allow',
+                  principal: [
+                    {
+                      key: 'AWS',
+                      value: ['*'],
+                    },
+                  ],
+                },
+              ],
+            }
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Lambda_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.FAIL)
+    })
+
+    test('Should pass when the policy has a DENY {AWS: `*`} principal', async () => {
+      const data = {
+        queryawsLambda: [
+          {
+            id: cuid(),
+            policy: {
+              statement: [
+                {
+                  effect: 'Deny',
+                  principal: [
+                    {
+                      key: 'AWS',
+                      value: ['*'],
+                    },
+                  ],
+                },
+              ],
+            }
+          },
+        ],
+      }
+
+      const [processedRule] = await rulesEngine.processRule(
+        Aws_PCI_DSS_321_Lambda_1 as Rule,
+        { ...data } as any
+      )
+
+      expect(processedRule.result).toBe(Result.PASS)
+    })
+  })
   describe('Lambda Check 2: Lambda functions should be in a VPC', () => {
     test('Should pass when it does have a vpcId', async () => {
       const data = {
