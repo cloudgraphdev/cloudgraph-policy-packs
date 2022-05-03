9f66ca4053c513b085c6750307f3f68e1bbaf3da
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-05-03 17:07:19 -0300

    feat: Included 1.x rules for aws cis 1.4.0

--- src/aws/cis-1.4.0/rules/index.ts
+++ src/aws/cis-1.4.0/rules/index.ts
@@ -1,4 +1,34 @@
 
+import Aws_CIS_140_14 from './aws-cis-1.4.0-1.4'
+import Aws_CIS_140_15 from './aws-cis-1.4.0-1.5'
+import Aws_CIS_140_16 from './aws-cis-1.4.0-1.6'
+import Aws_CIS_140_17 from './aws-cis-1.4.0-1.7'
+import Aws_CIS_140_18 from './aws-cis-1.4.0-1.8'
+import Aws_CIS_140_19 from './aws-cis-1.4.0-1.9'
+import Aws_CIS_140_110 from './aws-cis-1.4.0-1.10'
+import Aws_CIS_140_112 from './aws-cis-1.4.0-1.12'
+import Aws_CIS_140_113 from './aws-cis-1.4.0-1.13'
+import Aws_CIS_140_114 from './aws-cis-1.4.0-1.14'
+import Aws_CIS_140_115 from './aws-cis-1.4.0-1.15'
+import Aws_CIS_140_116 from './aws-cis-1.4.0-1.16'
+import Aws_CIS_140_117 from './aws-cis-1.4.0-1.17'
+import Aws_CIS_140_119 from './aws-cis-1.4.0-1.19'
+import Aws_CIS_140_121 from './aws-cis-1.4.0-1.21'
+
 export default [
-  // TODO: Add rules to export
+  Aws_CIS_140_14,
+  Aws_CIS_140_15,
+  Aws_CIS_140_16,
+  Aws_CIS_140_17,
+  Aws_CIS_140_18,
+  Aws_CIS_140_19,
+  Aws_CIS_140_110,
+  Aws_CIS_140_112,
+  Aws_CIS_140_113,
+  Aws_CIS_140_114,
+  Aws_CIS_140_115,
+  Aws_CIS_140_116,
+  Aws_CIS_140_117,
+  Aws_CIS_140_119,
+  Aws_CIS_140_121,
 ]
