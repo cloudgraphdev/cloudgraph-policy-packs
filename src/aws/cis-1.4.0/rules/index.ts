2ab813bfb6c10cddeca852ab3364f1c247ebb58a
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-05-05 17:09:22 -0300

    feat: Included 5.1, 5.2, 5.3 rules for aws cis 1.4.0

--- src/aws/cis-1.4.0/rules/index.ts
+++ src/aws/cis-1.4.0/rules/index.ts
@@ -1,4 +1,9 @@
+import Aws_CIS_140_51 from './aws-cis-1.4.0-5.1'
+import Aws_CIS_140_52 from './aws-cis-1.4.0-5.2'
+import Aws_CIS_140_53 from './aws-cis-1.4.0-5.3'
 
 export default [
-  // TODO: Add rules to export
+  Aws_CIS_140_51,
+  Aws_CIS_140_52,
+  Aws_CIS_140_53,
 ]
