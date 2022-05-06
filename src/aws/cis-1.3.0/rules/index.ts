4e263f23d08dcdcf97adf56ce0d72f9123249c9b
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-05-06 12:36:53 -0300

    feat: Included 3.x rules for aws cis 1.3.0

--- src/aws/cis-1.3.0/rules/index.ts
+++ src/aws/cis-1.3.0/rules/index.ts
@@ -1,4 +1,25 @@
+import Aws_CIS_130_31 from './aws-cis-1.3.0-3.1'
+import Aws_CIS_130_32 from './aws-cis-1.3.0-3.2'
+import Aws_CIS_130_33 from './aws-cis-1.3.0-3.3'
+import Aws_CIS_130_34 from './aws-cis-1.3.0-3.4'
+import Aws_CIS_130_35 from './aws-cis-1.3.0-3.5'
+import Aws_CIS_130_36 from './aws-cis-1.3.0-3.6'
+import Aws_CIS_130_37 from './aws-cis-1.3.0-3.7'
+import Aws_CIS_130_38 from './aws-cis-1.3.0-3.8'
+import Aws_CIS_130_39 from './aws-cis-1.3.0-3.9'
+import Aws_CIS_130_310 from './aws-cis-1.3.0-3.10'
+import Aws_CIS_130_311 from './aws-cis-1.3.0-3.11'
 
 export default [
-  // TODO: Add rules to export
+  Aws_CIS_130_31,
+  Aws_CIS_130_32,
+  Aws_CIS_130_33,
+  Aws_CIS_130_34,
+  Aws_CIS_130_35,
+  Aws_CIS_130_36,
+  Aws_CIS_130_37,
+  Aws_CIS_130_38,
+  Aws_CIS_130_39,
+  Aws_CIS_130_310,
+  Aws_CIS_130_311,
 ]
