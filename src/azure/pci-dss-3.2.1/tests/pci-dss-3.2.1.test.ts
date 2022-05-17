6c1364777295b8c9f0eb5e7eaaaf57b81ea97495
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-05-17 17:52:13 -0300

    feat: Initial boilerplate for Azure PCI DSS v3.2.1 benchmark

---
+++ src/azure/pci-dss-3.2.1/tests/pci-dss-3.2.1.test.ts
@@ -0,0 +1,18 @@
+import CloudGraph, { Rule, Result, Engine } from '@cloudgraph/sdk'
+
+describe('PCI Data Security Standard: 3.2.1', () => {
+  let rulesEngine: Engine
+  beforeAll(() => {
+    rulesEngine = new CloudGraph.RulesEngine({
+      providerName: 'azure',
+      entityName: 'PCI',
+    })
+  })
+
+  // TODO: Change once we have real checks
+  describe("Dummy Check", () => {
+    test('Dummy Test', async () => {
+      expect('PASS').toBe(Result.PASS)
+    })
+  })
+})
