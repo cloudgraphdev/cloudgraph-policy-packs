b810b0f679ed41e7aea6730a705feb7124e11dea
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-07-26 17:18:18 -0300

    feat(checks): Support [AZURE NIST] Logging, Performance and Reliability, User and Role Management, and Using Updated Policies and Frameworks rules

---
+++ src/azure/nist-800-53-rev4/rules/azure-nist-800-53-rev4-2.2.ts
@@ -0,0 +1,85 @@
+export default {
+  id: 'azure-nist-800-53-rev4-2.2',  
+  title: 'Azure NIST 2.2 Key Vault logging should be enabled',
+  
+  description:
+    'Enable AuditEvent logging for key vault instances to ensure interactions with key vaults are logged and available.',
+
+  audit: `**From Azure Console**
+
+  1. Go to Key vaults
+  2. For each Key vault
+  3. Go to Diagnostic Logs
+  4. Click on Edit Settings
+  5. Ensure that Archive to a storage account is Enabled
+  6. Ensure that AuditEvent is checked and the retention days is set to 180 days or as appropriate
+
+  **Using Azure Command Line Interface 2.0**
+
+  List all key vaults
+      az keyvault list
+
+  For each keyvault id
+      az monitor diagnostic-settings list --resource <id>
+
+  Ensure that storageAccountId is set as appropriate. Also, ensure that category and days are set. One of the sample outputs is as below.
+
+  "logs": [
+    {
+      "category": "AuditEvent",
+      "enabled": true,
+      "retentionPolicy": {
+        "days": 180,
+        "enabled": true
+      }
+    }
+  ]`,
+
+  rationale:
+    'Monitoring how and when key vaults are accessed, and by whom enables an audit trail of interactions with confidential information, keys and certificates managed by Azure Keyvault. Enabling logging for Key Vault saves information in an Azure storage account that the user provides. This creates a new container named insights-logs-auditevent automatically for the specified storage account, and this same storage account can be used for collecting logs for multiple key vaults.',
+
+  remediation:
+    'Follow Microsoft Azure documentation and setup Azure Key Vault Logging.',
+
+  references: [
+    'https://docs.microsoft.com/en-us/azure/key-vault/key-vault-logging',
+    'https://docs.microsoft.com/en-us/azure/security/benchmarks/security-controls-v2-logging-threat-detection#lt-4-enable-logging-for-azure-resources',
+  ],
+  gql: `{
+    queryazureKeyVault {
+      id
+      __typename
+      diagnosticSettings {
+        logs {
+          category
+          retentionPolicyEnabled
+          retentionPolicyDays
+        }
+      }
+    }
+  }`,
+  resource: 'queryazureKeyVault[*]',
+  severity: 'medium',
+  conditions: {
+    path: '@.diagnosticSettings',
+    array_any: {
+      path: '[*].logs',
+      array_any: {
+        and: [
+          {
+            path: '[*].category',
+            equal: 'AuditEvent',
+          },
+          {
+            path: '[*].retentionPolicyEnabled',
+            equal: true,
+          },
+          {
+            path: '[*].retentionPolicyDays',
+            greaterThanInclusive: 180,
+          },
+        ], 
+      },
+    },
+  },
+}
--- src/azure/nist-800-53-rev4/tests/nist-800-53-rev4-2.x.test.ts
+++ src/azure/nist-800-53-rev4/tests/nist-800-53-rev4-2.x.test.ts
@@ -3,14 +3,52 @@ import cuid from 'cuid'
 import { initRuleEngine } from '../../../utils/test'
 
 import Azure_NIST_800_53_21 from '../rules/azure-nist-800-53-rev4-2.1'
+import Azure_NIST_800_53_22 from '../rules/azure-nist-800-53-rev4-2.2'
+import Azure_NIST_800_53_23 from '../rules/azure-nist-800-53-rev4-2.3'
+import Azure_NIST_800_53_24 from '../rules/azure-nist-800-53-rev4-2.4'
+import Azure_NIST_800_53_25 from '../rules/azure-nist-800-53-rev4-2.5'
+
+export interface Log {
+  category: string
+  retentionPolicyEnabled: boolean
+  retentionPolicyDays: number
+}
+
+export interface DiagnosticSetting {
+  logs: Log[]
+}
+
+export interface RetentionPolicy {
+  enabled: boolean
+  days: number
+}
+
+export interface ServerBlobAuditingPolicy {
+  state: string
+}
+
+export interface QueryazureSqlServer {
+  id: string
+  serverBlobAuditingPolicies: ServerBlobAuditingPolicy[]
+}
+
+export interface QueryazureKeyVault {
+  id: string
+  diagnosticSettings: DiagnosticSetting[]
+}
 
 export interface QueryazureLogProfile {
   id: string
-  categories: string[]
+  name?: string
+  locations?: string[]
+  categories?: string[]
+  retentionPolicy?: RetentionPolicy | null
 }
 
-export interface NIS2xQueryResponse {
+export interface NIST2xQueryResponse {
   queryazureLogProfile?: QueryazureLogProfile[]
+  queryazureKeyVault?: QueryazureKeyVault[]
+  queryazureSqlServer?: QueryazureSqlServer[]
 }
 
 describe('Azure NIST 800-53: Rev. 4', () => {
@@ -20,14 +58,12 @@ describe('Azure NIST 800-53: Rev. 4', () => {
   })
 
   describe('Azure NIST 2.1 Monitor audit profile should log all activities', () => {
-    const getTestRuleFixture = (
-      categories: string[]
-      ): NIS2xQueryResponse => {
+    const getTestRuleFixture = (categories: string[]): NIST2xQueryResponse => {
       return {
         queryazureLogProfile: [
           {
             id: cuid(),
-            categories
+            categories,
           },
         ],
       }
@@ -35,7 +71,7 @@ describe('Azure NIST 800-53: Rev. 4', () => {
 
     // Act
     const testRule = async (
-      data: NIS2xQueryResponse,
+      data: NIST2xQueryResponse,
       expectedResult: Result
     ): Promise<void> => {
       // Act
@@ -49,17 +85,264 @@ describe('Azure NIST 800-53: Rev. 4', () => {
     }
 
     test('No Security Issue when Monitor audit profile log all activities', async () => {
-      const data: NIS2xQueryResponse = getTestRuleFixture(['Action', 'Write', 'Delete'])
+      const data: NIST2xQueryResponse = getTestRuleFixture([
+        'Action',
+        'Write',
+        'Delete',
+      ])
       await testRule(data, Result.PASS)
     })
 
     test('Security Issue when Monitor audit profile not log all activities', async () => {
-      const data: NIS2xQueryResponse = getTestRuleFixture(['Action', 'Delete'])
+      const data: NIST2xQueryResponse = getTestRuleFixture(['Action', 'Delete'])
       await testRule(data, Result.FAIL)
     })
 
     test('Security Issue when Monitor audit profile log activities are empty', async () => {
-      const data: NIS2xQueryResponse = getTestRuleFixture([])
+      const data: NIST2xQueryResponse = getTestRuleFixture([])
+      await testRule(data, Result.FAIL)
+    })
+  })
+
+  describe('Azure NIST 2.2 Key Vault logging should be enabled', () => {
+    const getTestRuleFixture = (
+      category: string,
+      retentionPolicyEnabled: boolean,
+      retentionPolicyDays: number
+    ): NIST2xQueryResponse => {
+      return {
+        queryazureKeyVault: [
+          {
+            id: cuid(),
+            diagnosticSettings: [
+              {
+                logs: [
+                  {
+                    category,
+                    retentionPolicyEnabled,
+                    retentionPolicyDays,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      }
+    }
+
+    // Act
+    const testRule = async (
+      data: NIST2xQueryResponse,
+      expectedResult: Result
+    ): Promise<void> => {
+      // Act
+      const [processedRule] = await rulesEngine.processRule(
+        Azure_NIST_800_53_22 as Rule,
+        { ...data }
+      )
+
+      // Asserts
+      expect(processedRule.result).toBe(expectedResult)
+    }
+
+    test('No Security Issue when Key Vault logging is enabled', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture(
+        'AuditEvent',
+        true,
+        180
+      )
+      await testRule(data, Result.PASS)
+    })
+
+    test('Security Issue when Key Vault logging is not enabled', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture(
+        'AuditEvent',
+        false,
+        180
+      )
+      await testRule(data, Result.FAIL)
+    })
+
+    test('Security Issue when Key Vault logging retentionPolicyDays is less than 180', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture(
+        'AuditEvent',
+        true,
+        179
+      )
+      await testRule(data, Result.FAIL)
+    })
+
+    test('Security Issue when Key Vault logging is empty', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture('', false, 0)
+      const keyVaults = data.queryazureKeyVault?.[0] as QueryazureKeyVault
+      keyVaults.diagnosticSettings = []
+      await testRule(data, Result.FAIL)
+    })
+  })
+
+  describe('Azure NIST 2.3 Monitor log profile should be created', () => {
+    const getTestRuleFixture = (
+      enabled: boolean,
+      days: number
+    ): NIST2xQueryResponse => {
+      return {
+        queryazureLogProfile: [
+          {
+            id: cuid(),
+            retentionPolicy: {
+              enabled,
+              days,
+            },
+          },
+        ],
+      }
+    }
+
+    // Act
+    const testRule = async (
+      data: NIST2xQueryResponse,
+      expectedResult: Result
+    ): Promise<void> => {
+      // Act
+      const [processedRule] = await rulesEngine.processRule(
+        Azure_NIST_800_53_23 as Rule,
+        { ...data }
+      )
+
+      // Asserts
+      expect(processedRule.result).toBe(expectedResult)
+    }
+
+    test('No Security Issue when Monitor audit profile log is created', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture(true, 0)
+      await testRule(data, Result.PASS)
+    })
+
+    test('Security Issue when Monitor audit profile log is not created', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture(false, 0)
+      await testRule(data, Result.FAIL)
+    })
+
+    test('Security Issue when Monitor audit profile log is empty', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture(false, 0)
+      const logProfile = data.queryazureLogProfile?.[0] as QueryazureLogProfile
+      logProfile.retentionPolicy = null
+      await testRule(data, Result.FAIL)
+    })
+  })
+
+  describe('Azure NIST 2.4 Monitor log profile should have activity logs for global services and all regions', () => {
+    const getTestRuleFixture = (
+      name: string,
+      locations: string[]
+    ): NIST2xQueryResponse => {
+      return {
+        queryazureLogProfile: [
+          {
+            id: cuid(),
+            name,
+            locations
+          },
+        ],
+      }
+    }
+
+    // Act
+    const testRule = async (
+      data: NIST2xQueryResponse,
+      expectedResult: Result
+    ): Promise<void> => {
+      // Act
+      const [processedRule] = await rulesEngine.processRule(
+        Azure_NIST_800_53_24 as Rule,
+        { ...data }
+      )
+
+      // Asserts
+      expect(processedRule.result).toBe(expectedResult)
+    }
+
+    test('No Security Issue when Monitor log profile have activity logs for global services and all regions', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture('default',  [
+        'centralus',
+        'eastus',
+        'northcentralus',
+        'southcentralus',
+        'westus',
+        'francecentral',
+        'germanynorth',
+        'swedencentral',
+        'global',
+      ])
+      await testRule(data, Result.PASS)
+    })
+
+    test('Security Issue when Monitor log profile have activity logs for global services but not for all regions', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture('default',  [
+        'centralus',
+        'eastus',
+        'northcentralus',
+        'southcentralus',
+      ])
+      await testRule(data, Result.FAIL)
+    })
+
+    test('Security Issue when Monitor log profile not have activity logs for global services and all regions', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture('test',  [
+        'centralus',
+        'eastus',
+        'northcentralus',
+        'southcentralus',
+        'westus',
+        'francecentral',
+        'germanynorth',
+        'swedencentral',
+        'global',
+      ])
+      await testRule(data, Result.FAIL)
+    })
+  })
+
+  describe('Azure NIST 2.5 SQL Server auditing should be enabled', () => {
+    const getTestRuleFixture = (
+      state: string
+      ): NIST2xQueryResponse => {
+      return {
+        queryazureSqlServer: [
+          {
+            id: cuid(),
+            serverBlobAuditingPolicies: [
+              {
+                state
+              }
+            ]
+          },
+        ],
+      }
+    }
+
+    // Act
+    const testRule = async (
+      data: NIST2xQueryResponse,
+      expectedResult: Result
+    ): Promise<void> => {
+      // Act
+      const [processedRule] = await rulesEngine.processRule(
+        Azure_NIST_800_53_25 as Rule,
+        { ...data }
+      )
+
+      // Asserts
+      expect(processedRule.result).toBe(expectedResult)
+    }
+
+    test('No Security Issue when SQL Server auditing is enabled', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture('Enabled')
+      await testRule(data, Result.PASS)
+    })
+
+    test('Security Issue when SQL Server auditing is disabled', async () => {
+      const data: NIST2xQueryResponse = getTestRuleFixture('Disabled')
       await testRule(data, Result.FAIL)
     })
   })
