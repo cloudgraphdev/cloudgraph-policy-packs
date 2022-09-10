5524032104a71ffe9a37a54d6f08f424e38bda09
James Zhou <james.zhou@inspire11.com>
2022-09-09 22:58:05 -0500

    add export new rules

--- src/azure/pci-dss-3.2.1/rules/index.ts
+++ src/azure/pci-dss-3.2.1/rules/index.ts
@@ -1,3 +1,7 @@
+import Azure_PCI_DSS_321_Encryption_1 from './pci-dss-3.2.1-encryption-check-1'
+import Azure_PCI_DSS_321_Encryption_2 from './pci-dss-3.2.1-encryption-check-2'
+import Azure_PCI_DSS_321_Encryption_3 from './pci-dss-3.2.1-encryption-check-3'
+import Azure_PCI_DSS_321_Encryption_4 from './pci-dss-3.2.1-encryption-check-4'
 import Azure_PCI_DSS_321_Monitoring_1 from './pci-dss-3.2.1-monitoring-check-1'
 import Azure_PCI_DSS_321_Monitoring_2 from './pci-dss-3.2.1-monitoring-check-2'
 import Azure_PCI_DSS_321_Monitoring_3 from './pci-dss-3.2.1-monitoring-check-3'
@@ -5,6 +9,10 @@ import Azure_PCI_DSS_321_Networking_1 from './pci-dss-3.2.1-networking-check-1'
 import Azure_PCI_DSS_321_Networking_2 from './pci-dss-3.2.1-networking-check-2'
 
 export default [
+  Azure_PCI_DSS_321_Encryption_1,
+  Azure_PCI_DSS_321_Encryption_2,
+  Azure_PCI_DSS_321_Encryption_3,
+  Azure_PCI_DSS_321_Encryption_4,
   Azure_PCI_DSS_321_Monitoring_1,
   Azure_PCI_DSS_321_Monitoring_2,
   Azure_PCI_DSS_321_Monitoring_3,
