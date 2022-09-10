257254fa844482076302998f011ccccc369e9ad0
James Zhou <james.zhou@inspire11.com>
2022-09-09 23:06:18 -0500

    add export new rules

--- src/azure/pci-dss-3.2.1/rules/index.ts
+++ src/azure/pci-dss-3.2.1/rules/index.ts
@@ -1,13 +1,23 @@
 import Azure_PCI_DSS_321_Monitoring_1 from './pci-dss-3.2.1-monitoring-check-1'
 import Azure_PCI_DSS_321_Monitoring_2 from './pci-dss-3.2.1-monitoring-check-2'
 import Azure_PCI_DSS_321_Monitoring_3 from './pci-dss-3.2.1-monitoring-check-3'
+import Azure_PCI_DSS_321_Network_Access_1 from './pci-dss-3.2.1-network-access-check-1'
+import Azure_PCI_DSS_321_Network_Access_2 from './pci-dss-3.2.1-network-access-check-2'
+import Azure_PCI_DSS_321_Network_Access_3 from './pci-dss-3.2.1-network-access-check-3'
+import Azure_PCI_DSS_321_Network_Access_4 from './pci-dss-3.2.1-network-access-check-4'
 import Azure_PCI_DSS_321_Networking_1 from './pci-dss-3.2.1-networking-check-1'
 import Azure_PCI_DSS_321_Networking_2 from './pci-dss-3.2.1-networking-check-2'
+import Azure_PCI_DSS_321_Policy_Version_1 from './pci-dss-3.2.1-policy-version-check-1'
 
 export default [
   Azure_PCI_DSS_321_Monitoring_1,
   Azure_PCI_DSS_321_Monitoring_2,
   Azure_PCI_DSS_321_Monitoring_3,
+  Azure_PCI_DSS_321_Network_Access_1,
+  Azure_PCI_DSS_321_Network_Access_2,
+  Azure_PCI_DSS_321_Network_Access_3,
+  Azure_PCI_DSS_321_Network_Access_4,
   Azure_PCI_DSS_321_Networking_1,
   Azure_PCI_DSS_321_Networking_2,
+  Azure_PCI_DSS_321_Policy_Version_1,
 ]
