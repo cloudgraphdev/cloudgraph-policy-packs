b8eb5061bba7c7f9b9547aa097a995bc8fb63ab3
James Zhou <james.zhou@inspire11.com>
2022-09-09 22:50:29 -0500

    add export new rules

--- src/azure/pci-dss-3.2.1/rules/index.ts
+++ src/azure/pci-dss-3.2.1/rules/index.ts
@@ -3,6 +3,8 @@ import Azure_PCI_DSS_321_Monitoring_2 from './pci-dss-3.2.1-monitoring-check-2'
 import Azure_PCI_DSS_321_Monitoring_3 from './pci-dss-3.2.1-monitoring-check-3'
 import Azure_PCI_DSS_321_Networking_1 from './pci-dss-3.2.1-networking-check-1'
 import Azure_PCI_DSS_321_Networking_2 from './pci-dss-3.2.1-networking-check-2'
+import Azure_PCI_DSS_321_Networking_3 from './pci-dss-3.2.1-networking-check-3'
+import Azure_PCI_DSS_321_User_1 from './pci-dss-3.2.1-user-check-1'
 
 export default [
   Azure_PCI_DSS_321_Monitoring_1,
@@ -10,4 +12,6 @@ export default [
   Azure_PCI_DSS_321_Monitoring_3,
   Azure_PCI_DSS_321_Networking_1,
   Azure_PCI_DSS_321_Networking_2,
+  Azure_PCI_DSS_321_Networking_3,
+  Azure_PCI_DSS_321_User_1,
 ]
