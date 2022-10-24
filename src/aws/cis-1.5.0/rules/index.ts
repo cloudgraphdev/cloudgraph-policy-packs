46c94833fae665b9ab12482a247ff5a63d03fc42
James Zhou <james.zhou@inspire11.com>
2022-10-23 22:46:40 -0500

    feat(CG-1282): add support monitoring rules

--- src/aws/cis-1.5.0/rules/index.ts
+++ src/aws/cis-1.5.0/rules/index.ts
@@ -1,2 +1,33 @@
+import Aws_CIS_150_41 from './aws-cis-1.5.0-4.1'
+import Aws_CIS_150_42 from './aws-cis-1.5.0-4.2'
+import Aws_CIS_150_43 from './aws-cis-1.5.0-4.3'
+import Aws_CIS_150_44 from './aws-cis-1.5.0-4.4'
+import Aws_CIS_150_45 from './aws-cis-1.5.0-4.5'
+import Aws_CIS_150_46 from './aws-cis-1.5.0-4.6'
+import Aws_CIS_150_47 from './aws-cis-1.5.0-4.7'
+import Aws_CIS_150_48 from './aws-cis-1.5.0-4.8'
+import Aws_CIS_150_49 from './aws-cis-1.5.0-4.9'
+import Aws_CIS_150_410 from './aws-cis-1.5.0-4.10'
+import Aws_CIS_150_411 from './aws-cis-1.5.0-4.11'
+import Aws_CIS_150_412 from './aws-cis-1.5.0-4.12'
+import Aws_CIS_150_413 from './aws-cis-1.5.0-4.13'
+import Aws_CIS_150_414 from './aws-cis-1.5.0-4.14'
+import Aws_CIS_150_415 from './aws-cis-1.5.0-4.15'
+
 export default [
+  Aws_CIS_150_41,
+  Aws_CIS_150_42,
+  Aws_CIS_150_43,
+  Aws_CIS_150_44,
+  Aws_CIS_150_45,
+  Aws_CIS_150_46,
+  Aws_CIS_150_47,
+  Aws_CIS_150_48,
+  Aws_CIS_150_49,
+  Aws_CIS_150_410,
+  Aws_CIS_150_411,
+  Aws_CIS_150_412,
+  Aws_CIS_150_413,
+  Aws_CIS_150_414,
+  Aws_CIS_150_415,
 ]
